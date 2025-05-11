c8fa8c04b4e82e7f611d7b694225e45c9418558a
Hardpansara <hardpansara10@gmail.com>
2025-05-11 18:45:57 +0530

    images added

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,11 +1,11 @@
 class AppConfig {
   // TODO: Replace with your Infura project ID
-  static const String infuraProjectId = 'YOUR-API';
+  static const String infuraProjectId = '104bae610fd5440d8efbf539024e3b28';
   
   static const Map<String, String> networks = {
-    'mainnet': 'YOUR-API',
-    'goerli': 'YOUR-API',
-    'sepolia': 'YOUR-API',
+    'mainnet': 'https://mainnet.infura.io/v3/$infuraProjectId',
+    'goerli': 'https://goerli.infura.io/v3/$infuraProjectId',
+    'sepolia': 'https://sepolia.infura.io/v3/$infuraProjectId',
   };
 
   static const String defaultNetwork = 'sepolia'; // Using testnet by default
@@ -14,7 +14,7 @@ class AppConfig {
   // m/44'/60'/0'/0/0 for Ethereum
   static const String derivationPath = "m/44'/60'/0'/0/0";
 
-  static const etherscanApiKey = 'YOUR-API';
+  static const etherscanApiKey = 'FSFRAPG1QJIZKGY2PNE89IHY6YR7A1WVX6';
   
   static const Map<String, String> etherscanApis = {
     'mainnet': 'https://api.etherscan.io/api',
